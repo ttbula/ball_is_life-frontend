3de6fac1b767f4d9394c5d54bb529399322b5500
mo4rahman <mo4rahman@gmail.com>
2022-05-17 14:31:44 -0400

    up to date with migration and updating separate git repos for our front end and backend

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import Rosters from "./pages/Rosters";
 import ErrorPage from "./pages/ErrorPage";
 
 function App() {
+  const URL = "https://ballis-life.herokuapp.com/";
   return (
     <Router>
       <nav>
         <Link to="/"> Home </Link>
         <Link to="/"> Create Player </Link>
+        <Link to="/rosters"> Test </Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home URL={URL} />} />
         <Route path="/player/:playername" element={<Player />} />
-        <Route path="/rosters" element={<Rosters />} />
+        <Route path="/rosters" element={<Rosters URL={URL} />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <div className="footer">Footer Info</div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-// const cors = require("cors");
 
 function Home() {
   const url =
     "https://cors-anywhere.herokuapp.com/https://balldontlie.io/api/v1/teams";
-  //   const url = "https://balldontlie.io/api/v1/players?page=151";
   const [list, setList] = useState();
   async function fetchData() {
     const response = await fetch(url);
@@ -18,12 +16,26 @@ function Home() {
   }, []);
 
   return (
+    //FIXME: Running into too many requests. need to save information into mongoDB
     <div>
-      <h2>Home Page of all Teams</h2>
+      <h1>Home Page of all Teams</h1>
+      <h2>Eastern Conference</h2>
       {list && (
         <>
           {list.map((team, index) => {
-            return <div key={index}>{team.full_name}</div>;
+            if (team.conference === "East") {
+              return <div key={index}>{team.full_name}</div>;
+            }
+          })}
+        </>
+      )}
+      <h2>Western Conference</h2>
+      {list && (
+        <>
+          {list.map((team, index) => {
+            if (team.conference === "West") {
+              return <div key={index}>{team.full_name}</div>;
+            }
           })}
         </>
       )}
