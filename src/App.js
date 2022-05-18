da1f6586ba4b6fcc123525b0a5ca8dc4773d79e4
mo4rahman <mo4rahman@gmail.com>
2022-05-18 11:28:03 -0400

    customizing css

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
         <Link to="/">
           {" "}
           <img
+            id="jordan"
             src="https://cdn-icons.flaticon.com/png/512/3177/premium/3177098.png?token=exp=1652887545~hmac=50c1b54bb01aab435421c6ac1baff1c1"
             alt="michael jordan dunking"
           />{" "}
@@ -50,7 +51,7 @@ function App() {
         />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-      <div className="footer">Footer Info</div>
+      <div className="footer">Tim & Mohammed</div>
     </Router>
   );
 }
